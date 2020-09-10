8245e74dfaf768638db93853d90f616668884e0c
bri25yu <bri25yu@gmail.com>
2020-09-10 11:24:20 -0700

    moving files around so that katt template works

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-import Blog from "./components/Blog";
+// import Blog from "./components/Blog";
 
 
-const container = document.getElementById("index");
-ReactDOM.render(React.createElement(Blog, null, "Currently revamping, please check back later!"), container);
+// const container = document.getElementById("index");
+// ReactDOM.render(React.createElement(Blog), container);
