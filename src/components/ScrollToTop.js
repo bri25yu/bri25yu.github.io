f3cb2f27e18716f37ebe52ae57307cb33a025c62
bri25yu <bri25yu@gmail.com>
2020-09-10 12:14:29 -0700

    moving around katt html code into some js files

---
+++ src/components/ScrollToTop.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+
+export default function ScrollToTop(props) {
+    return <div class="fixed scroll-top">
+            <img src="images/back_to_top.png" alt="Go Top" />
+        </div>
+}
