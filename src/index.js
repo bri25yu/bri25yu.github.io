ace02bce4317b629fe4f01457312dbb7b7b4d319
bri25yu <bri25yu@gmail.com>
2020-09-09 23:59:32 -0700

    adding revamping message

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from "react-dom";
 
 
 const container = document.getElementById("index");
-ReactDOM.render(React.createElement("div"), container);
+ReactDOM.render(React.createElement("div", null, "Currently revamping, please check back later!"), container);
