47985b30492f755b09b75bf31b4a2834d82c885a
bri25yu <bri25yu@gmail.com>
2020-09-10 14:03:04 -0700

    removing katt template code, copying style myself instead

--- src/components/ScrollToTop.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-
-
-export default function ScrollToTop(props) {
-    return <div class="fixed scroll-top">
-            <img src="images/back_to_top.png" alt="Go Top" />
-        </div>
-}
---
+++ src/constants.js
@@ -0,0 +1,16 @@
+export const EL = {
+    FOOTER: "footer",
+    DIV: "div",
+    UL: "ul",
+    LI: "li",
+}
+
+export const ATTR = {
+    CLASSNAME = "className",
+}
+
+export const CLASS = {
+    CONTENT_1170: "content-1170",
+    CENTER_RELATIVE: "center-relative",
+    LEFT_FOOTER_CONTENT: "left-footer-content",
+}
